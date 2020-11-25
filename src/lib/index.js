0d0e1d03d79786e338dafe93f42e84a7a09c5282
Jesse Mykolyn <jrmykolyn@gmail.com>
2020-11-25 01:36:44 -0500

    Add suport for normalization and basic resolution

--- src/lib/index.js
+++ src/lib/index.js
@@ -20,8 +20,108 @@ const sanitize = (data) => {
     .reduce((acc, o) => ({ ...acc, ...o }), {});
 };
 
+/**
+ * Given an object of sanitized architecture data, update it such that primitive
+ * dependencies are converted into their 'artifacts' equivalent.
+ * is keyed by name. For example:
+ *
+ * // Input
+ * {
+ *   "applications": {
+ *     "foo": {
+ *       "name": "foo",
+ *       "dependencies": ["bar"]
+ *     }
+ *   },
+ *   "artifacts": {
+ *     { "name": "bar", "type": "library" }
+ *   ]
+ * }
+ *
+ * // Output
+ * {
+ *   "applications": {
+ *     "foo": {
+ *       "dependencies": [
+ *         { "name": "bar", "type": "library" }
+ *       ]
+ *     }
+ *   },
+ *   "artifacts": {
+ *     { "name": "bar", "type": "library" }
+ *   ]
+ * }
+ */
+const intermediate = ({ artifacts, ...rest }) => {
+  return Object.entries(rest)
+    .map(([key, value]) => ({
+      [key]: Object.entries(value)
+        .map(([k, { dependencies = [], ...r }]) => ({
+          [k]: {
+            ...r,
+            dependencies: dependencies.map((dependency) => artifacts.find(({ name }) => name === dependency)),
+          }
+        }))
+        .reduce((acc, o) => ({ ...acc, ...o }), {}),
+    }))
+    .reduce((acc, o) => ({ ...acc, ...o }), { artifacts });
+};
+
+/**
+ * Given an object of sanitize artifact data, generate and return a fully
+ * resolved dependency tree. For example:
+ *
+ * // Input
+ * {
+ *   "applications": {
+ *     "foo": {
+ *       "name": "foo",
+ *       "dependencies": ["bar"]
+ *     }
+ *   },
+ *   "libraries": {
+ *     "bar": {
+ *       "name": "bar"
+ *     }
+ *   }
+ * }
+ *
+ * // Output
+ * {
+ *   "applications": {
+ *     "foo": {
+ *       "dependencies": [
+ *         "bar": {
+ *           "name": "bar"
+ *         }
+ *       ]
+ *     }
+ *   },
+ *   "libraries": {
+ *     "bar": {
+ *       "name": "bar"
+ *     }
+ *   }
+ * }
+ */
+const resolve = ({ artifacts, ...rest }) => {
+  return Object.entries(rest)
+    .map(([key, value]) => ({
+      [key]: Object.values(value)
+      .map(({ dependencies, ...r }) => ({
+        ...r,
+        dependencies: dependencies.map(({ name, type }) => rest[type][name]),
+      })),
+    }))
+    .reduce((acc, o) => ({ ...acc, ...o }), {});
+};
+
 const sanitizedData = Object.entries(rest)
   .map(([key, value]) => ({ [key]: sanitize(value) }))
   .reduce((acc, o) => ({ ...acc, ...o }), { artifacts });
 
-console.log(sanitizedData);
+const intermediateData = intermediate(sanitizedData);
+
+const resolvedData = resolve(intermediateData);
+
+console.log(JSON.stringify(resolvedData, null, 2));
